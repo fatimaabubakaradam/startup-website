4c7e5416e51e4417ab772faf4a7b59f1d6c6f6eb
fatimaabubakaradam <zahrammcy@gmail.com>
2024-11-07 16:50:01 -0800

    add css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-// import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import Header from './header';
+import Header from './header'; 
 import Social from './social';
 import Team from './team';
+import Mail from './mail';  
 import Cloud from './cloud';
 import Footer from './footer';
-
-
 
 const App = () => {
   return (
@@ -17,6 +15,7 @@ const App = () => {
         <Social />
         <Team />
         <Cloud />
+        <Mail />  
       </main>
       <Footer />
     </div>
