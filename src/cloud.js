7f71ddd4a3a940e2676e3e8f2eefb049a8e14706
fatimaabubakaradam <zahrammcy@gmail.com>
2024-11-07 17:13:38 -0800

    add mail component

--- src/cloud.js
+++ src/cloud.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const cloud = () => {
   return (
-    <div></div>
+    <div className='talent'>
+      <h3 className='teeam'>Team</h3>
+      <h1 className='our'>Our Talents</h1>
+      <h3 className='amet'>Lorem ipsum, dolor sit amet consectetur adipisicing elit</h3>
+    </div>
   );
 };
 
--- src/mail.js
+++ src/mail.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import mailImage from './asset/mail.png'; 
 
-const mail = () => {
+const Mail = () => { 
   return (
-    <div></div>
+    <div>
+      <img src={mailImage} alt="mail" className='mail' />
+      <h1 className='team-text1'>Lemon ipsum dolor sit amet consectetur</h1>
+      <h3 className='team-text'>Lemon ipsum, dolor sit amet consectetur adipisicing elit. Suscipit  meno hit quos, ab, dolor aperiam nobis cum est eos error ipsum,volimptate culpa nesciunt delectus  iste?</h3>
+      <button className="learn-button1">Learn More</button>
+    </div>
   );
 };
 
-export default mail;
+export default Mail;
