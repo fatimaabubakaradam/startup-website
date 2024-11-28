3bf90df3325e645edb9f9568a2a3e90a2c41105f
fatimaabubakaradam <zahrammcy@gmail.com>
2024-11-28 14:55:31 -0800

    add animation

--- src/name.js
+++ src/name.js
@@ -1,33 +1,36 @@
 import React from 'react';
-import Rec from "./asset/Rectangle 249 (2).png"
-import Rec1 from "./asset/Rectangle 250.png"
-import Whether from "./asset/ghhgnhg 1.png"
+import Whether from "./asset/ghhgnhg 1.png";
+import { motion } from 'framer-motion';
 
 const Name = () => {
   return (
-    <> 
-    <div className='contact-line'>
-    <div className='whe-container'><img src={Whether} alt='whether' className='whether' /></div>
-    <div className="contact-form">
-      <form>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name"  />
+    <motion.div
+      className='contact-line'
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 2 }}
+    >
+      <div className='whe-container'>
+        <img src={Whether} alt='whether' className='whether' />
+      </div>
+      <div className="contact-form">
+        <form>
+          <label>Name</label>
+          <input type="text" />
 
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email"  />
+          <label>Email</label>
+          <input type="email" />
 
-        <label htmlFor="message">Message</label>
-        <textarea id="message" ></textarea>
+          <label>Message</label>
+          <textarea></textarea>
 
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-    </div>
-   
-    </>
-    
-  )
-  
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+
+     
+    </motion.div>
+  );
 };
 
 export default Name;
